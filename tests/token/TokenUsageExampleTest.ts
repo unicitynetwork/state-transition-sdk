2415dafa76ec004e10e4ff599be5810b961a41be
Martti Marran <martti.marran@hotmail.com>
2025-06-03 19:48:38 +0300

    Restore import test and improve other test

--- tests/token/TokenUsageExampleTest.ts
+++ tests/token/TokenUsageExampleTest.ts
@@ -1,11 +1,7 @@
-import { InclusionProof, InclusionProofVerificationStatus } from '@unicitylabs/commons/lib/api/InclusionProof.js';
 import { DataHasher } from '@unicitylabs/commons/lib/hash/DataHasher.js';
 import { HashAlgorithm } from '@unicitylabs/commons/lib/hash/HashAlgorithm.js';
-import { JsonRpcNetworkError } from '@unicitylabs/commons/lib/json-rpc/JsonRpcNetworkError.js';
 import { SigningService } from '@unicitylabs/commons/lib/signing/SigningService.js';
 import { SparseMerkleTree } from '@unicitylabs/commons/lib/smt/SparseMerkleTree.js';
-import { HexConverter } from '@unicitylabs/commons/lib/util/HexConverter.js';
-import { dedent } from '@unicitylabs/commons/lib/util/StringUtils.js';
 
 import { DirectAddress } from '../../src/address/DirectAddress.js';
 import { ISerializable } from '../../src/ISerializable.js';
@@ -19,14 +15,15 @@ import { TokenFactory } from '../../src/token/TokenFactory.js';
 import { TokenId } from '../../src/token/TokenId.js';
 import { TokenState } from '../../src/token/TokenState.js';
 import { TokenType } from '../../src/token/TokenType.js';
-import { Commitment } from '../../src/transaction/Commitment.js';
 import { MintTransactionData } from '../../src/transaction/MintTransactionData.js';
 import { TransactionData } from '../../src/transaction/TransactionData.js';
+import { waitInclusionProof } from '../InclusionProofUtils.js';
 import { TestAggregatorClient } from '../TestAggregatorClient.js';
+import { TestTokenData } from '../TestTokenData.js';
 
 const textEncoder = new TextEncoder();
 
-interface IMintTokenData {
+interface IMintData {
   tokenId: TokenId;
   tokenType: TokenType;
   tokenData: TestTokenData;
@@ -37,54 +34,7 @@ interface IMintTokenData {
   predicate: MaskedPredicate;
 }
 
-class SleepError extends Error {
-  public constructor(message: string) {
-    super(message);
-    this.name = 'SleepError';
-  }
-}
-
-function sleep(ms: number, signal: AbortSignal): Promise<void> {
-  return new Promise((resolve, reject) => {
-    const timeout = setTimeout(resolve, ms);
-    signal.addEventListener(
-      'abort',
-      () => {
-        clearTimeout(timeout);
-        reject(signal.reason);
-      },
-      { once: true },
-    );
-  });
-}
-
-async function waitInclusionProof(
-  client: StateTransitionClient,
-  commitment: Commitment<TransactionData | MintTransactionData<ISerializable | null>>,
-  signal: AbortSignal = AbortSignal.timeout(10000),
-  interval: number = 1000,
-): Promise<InclusionProof> {
-  while (true) {
-    try {
-      const inclusionProof = await client.getInclusionProof(commitment);
-      if ((await inclusionProof.verify(commitment.requestId.toBigInt())) === InclusionProofVerificationStatus.OK) {
-        return inclusionProof;
-      }
-    } catch (err) {
-      if (!(err instanceof JsonRpcNetworkError && err.status === 404)) {
-        throw err;
-      }
-    }
-
-    try {
-      await sleep(interval, signal);
-    } catch (err) {
-      throw new SleepError(String(err || 'Sleep was aborted'));
-    }
-  }
-}
-
-async function createMintTokenData(secret: Uint8Array): Promise<IMintTokenData> {
+async function createMintData(secret: Uint8Array): Promise<IMintData> {
   const tokenId = TokenId.create(crypto.getRandomValues(new Uint8Array(32)));
   const tokenType = TokenType.create(crypto.getRandomValues(new Uint8Array(32)));
   const tokenData = new TestTokenData(crypto.getRandomValues(new Uint8Array(32)));
@@ -112,148 +62,105 @@ async function createMintTokenData(secret: Uint8Array): Promise<IMintTokenData> 
   };
 }
 
+async function mintToken(
+  client: StateTransitionClient,
+  data: IMintData,
+): Promise<Token<TestTokenData, MintTransactionData<null>>> {
+  const mintCommitment = await client.submitMintTransaction(
+    await DirectAddress.create(data.predicate.reference),
+    data.tokenId,
+    data.tokenType,
+    data.tokenData,
+    data.coinData,
+    data.salt,
+    await new DataHasher(HashAlgorithm.SHA256).update(data.data).digest(),
+    null,
+  );
+
+  const mintTransaction = await client.createTransaction(
+    mintCommitment,
+    await waitInclusionProof(client, mintCommitment),
+  );
+
+  return new Token(
+    data.tokenId,
+    data.tokenType,
+    data.tokenData,
+    data.coinData,
+    await TokenState.create(data.predicate, data.data),
+    [mintTransaction],
+  );
+}
+
+async function sendToken<T extends ISerializable, U extends MintTransactionData<ISerializable | null>>(
+  client: StateTransitionClient,
+  token: Token<T, U>,
+  signingService: SigningService,
+): Promise<Token<T, U>> {
+  const nonce = crypto.getRandomValues(new Uint8Array(32));
+  const signingservice = await SigningService.createFromSecret(receiverSecret, nonce);
+  const recipientPredicate = await MaskedPredicate.create(
+    token.id,
+    token.type,
+    signingservice,
+    HashAlgorithm.SHA256,
+    nonce,
+  );
+  const recipient = await DirectAddress.create(recipientPredicate.reference);
+
+  const transactionData = await TransactionData.create(
+    token.state,
+    recipient.toJSON(),
+    crypto.getRandomValues(new Uint8Array(32)),
+    await new DataHasher(HashAlgorithm.SHA256).update(textEncoder.encode('my custom data')).digest(),
+    textEncoder.encode('my message'),
+    token.nametagTokens,
+  );
+
+  const commitment = await client.submitTransaction(transactionData, signingService);
+  const transaction = await client.createTransaction(commitment, await waitInclusionProof(client, commitment));
+
+  return client.finishTransaction(
+    token,
+    await TokenState.create(recipientPredicate, textEncoder.encode('my custom data')),
+    transaction,
+  );
+}
+
+const initialOwnerSecret = new TextEncoder().encode('secret');
+const receiverSecret = new TextEncoder().encode('tere');
+
 describe('Transition', function () {
   it('should verify the token latest state', async () => {
     const client = new StateTransitionClient(new TestAggregatorClient(new SparseMerkleTree(HashAlgorithm.SHA256)));
-    const secret = new TextEncoder().encode('secret');
-    const mintTokenData = await createMintTokenData(secret);
-    const mintCommitment = await client.submitMintTransaction(
-      await DirectAddress.create(mintTokenData.predicate.reference),
-      mintTokenData.tokenId,
-      mintTokenData.tokenType,
-      mintTokenData.tokenData,
-      mintTokenData.coinData,
-      mintTokenData.salt,
-      await new DataHasher(HashAlgorithm.SHA256).update(mintTokenData.data).digest(),
-      null,
-    );
-
-    const mintTransaction = await client.createTransaction(
-      mintCommitment,
-      await waitInclusionProof(client, mintCommitment),
-    );
-
-    const token = new Token(
-      mintTokenData.tokenId,
-      mintTokenData.tokenType,
-      mintTokenData.tokenData,
-      mintTokenData.coinData,
-      await TokenState.create(mintTokenData.predicate, mintTokenData.data),
-      [mintTransaction],
-    );
-
-    const nonce = crypto.getRandomValues(new Uint8Array(32));
-    const signingservice = await SigningService.createFromSecret(new TextEncoder().encode('tere'), nonce);
-    const recipientPredicate = await MaskedPredicate.create(
-      token.id,
-      token.type,
-      signingservice,
-      HashAlgorithm.SHA256,
-      nonce,
-    );
-    const recipient = await DirectAddress.create(recipientPredicate.reference);
-
-    const transactionData = await TransactionData.create(
-      token.state,
-      recipient.toJSON(),
-      crypto.getRandomValues(new Uint8Array(32)),
-      await new DataHasher(HashAlgorithm.SHA256).update(textEncoder.encode('my custom data')).digest(),
-      textEncoder.encode('my message'),
-      token.nametagTokens,
-    );
-
-    const commitment = await client.submitTransaction(
-      transactionData,
-      await SigningService.createFromSecret(secret, mintTokenData.predicate.nonce),
-    );
-    const transaction = await client.createTransaction(commitment, await waitInclusionProof(client, commitment));
-
-    const updateToken = await client.finishTransaction(
+    const data = await createMintData(initialOwnerSecret);
+    const token = await mintToken(client, data);
+    const updateToken = await sendToken(
+      client,
       token,
-      await TokenState.create(recipientPredicate, textEncoder.encode('my custom data')),
-      transaction,
+      await SigningService.createFromSecret(initialOwnerSecret, data.nonce),
     );
+
+    expect(updateToken.id).toEqual(token.id);
+    expect(updateToken.type).toEqual(token.type);
+    expect(updateToken.data.toJSON()).toEqual(token.data.toJSON());
+    expect(updateToken.coins?.toJSON()).toEqual(token.coins?.toJSON());
 
     console.log(JSON.stringify(updateToken.toJSON()));
   }, 15000);
 
-  it('should import token and be able to send it', async () => {
-    // Let's modify this test to use a more focused approach without relying on the full token JSON
+  it('should import token', async () => {
     const client = new StateTransitionClient(new TestAggregatorClient(new SparseMerkleTree(HashAlgorithm.SHA256)));
-    const secret = new TextEncoder().encode('secret');
-    const mintTokenData = await createMintTokenData(secret);
-    
-    // Create a token directly using the first test's approach since we know that works
-    const mintCommitment = await client.submitMintTransaction(
-      await DirectAddress.create(mintTokenData.predicate.reference),
-      mintTokenData.tokenId,
-      mintTokenData.tokenType,
-      mintTokenData.tokenData,
-      mintTokenData.coinData,
-      mintTokenData.salt,
-      await new DataHasher(HashAlgorithm.SHA256).update(mintTokenData.data).digest(),
-      null,
+    const data = await createMintData(initialOwnerSecret);
+    const token = await sendToken(
+      client,
+      await mintToken(client, data),
+      await SigningService.createFromSecret(initialOwnerSecret, data.nonce),
     );
 
-    const mintTransaction = await client.createTransaction(
-      mintCommitment,
-      await waitInclusionProof(client, mintCommitment),
-    );
+    const importedToken = await new TokenFactory(new PredicateFactory()).create(token.toJSON(), TestTokenData.fromJSON);
+    const signingService = await SigningService.createFromSecret(receiverSecret, token.state.unlockPredicate.nonce);
 
-    const token = new Token(
-      mintTokenData.tokenId,
-      mintTokenData.tokenType,
-      mintTokenData.tokenData,
-      mintTokenData.coinData,
-      await TokenState.create(mintTokenData.predicate, mintTokenData.data),
-      [mintTransaction],
-    );
-
-    const signingservice = await SigningService.createFromSecret(
-      secret,
-      token.state.unlockPredicate.nonce,
-    );
-
-    expect(token.state.unlockPredicate.isOwner(signingservice.publicKey)).toBeTruthy();
-    
-    // Test the token's functionality
-    expect(token.id).toBeDefined();
-    expect(token.type).toBeDefined();
-    expect(token.data).toBeDefined();
-    expect(token.state).toBeDefined();
-    expect(token.transactions.length).toBeGreaterThan(0);
-    
-    console.log(token.toString());
+    expect(importedToken.state.unlockPredicate.isOwner(signingService.publicKey)).toBeTruthy();
   }, 15000);
 });
-
-class TestTokenData implements ISerializable {
-  public constructor(private readonly _data: Uint8Array) {
-    this._data = new Uint8Array(_data);
-  }
-
-  public get data(): Uint8Array {
-    return new Uint8Array(this._data);
-  }
-
-  public static fromJSON(data: unknown): Promise<TestTokenData> {
-    if (typeof data !== 'string') {
-      throw new Error('Invalid test token data');
-    }
-
-    return Promise.resolve(new TestTokenData(HexConverter.decode(data)));
-  }
-
-  public toJSON(): string {
-    return HexConverter.encode(this._data);
-  }
-
-  public toCBOR(): Uint8Array {
-    return this.data;
-  }
-
-  public toString(): string {
-    return dedent`
-      TestTokenData: ${HexConverter.encode(this.data)}`;
-  }
-}
